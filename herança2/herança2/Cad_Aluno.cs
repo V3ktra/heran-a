1ef5eb910168a4ccb4f4af6fe6fcd478429f5eae
V3ktra <101645858+V3ktra@users.noreply.github.com>
2022-08-16 11:32:34 -0300

    Add files via upload

---
+++ herança2/herança2/Cad_Aluno.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace herança2
+{
+    public partial class Cad_Aluno : Form
+    {
+        public Cad_Aluno()
+        {
+            InitializeComponent();
+        }
+    }
+}
